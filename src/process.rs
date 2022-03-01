5418363400c168dfa8746f3b30d11728c6435d73
Leo Lu <luyuxuanleo@gmail.com>
2022-03-01 22:26:17 +0800

    Slow implement

--- src/process.rs
+++ src/process.rs
@@ -1,27 +1,85 @@
-use dashmap::DashMap;
+use fxhash::{FxHashMap, FxHashSet};
 use indicatif::{ProgressBar, ProgressStyle};
+use lasso::{Key, Rodeo, RodeoReader};
+use petgraph::graph::{Graph, NodeIndex};
+use petgraph::prelude::*;
+use petgraph::Directed;
 use rayon::prelude::*;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fmt;
 use std::fs;
 use std::io::{BufWriter, Read, Write};
+use std::num::NonZeroU32;
+
+// First make our key type, this will be what we use as handles into our interner
+#[derive(Copy, Clone, PartialEq, Eq, Debug, Hash)]
+struct NicheKey(NonZeroU32);
+
+impl Default for NicheKey {
+    fn default() -> Self {
+        NicheKey(NonZeroU32::new(1).unwrap())
+    }
+}
+
+unsafe impl Key for NicheKey {
+    #[cfg_attr(feature = "inline-more", inline)]
+    fn into_usize(self) -> usize {
+        self.0.get() as usize - 1
+    }
+
+    /// Returns `None` if `int` is greater than `u32::MAX - 1`
+    #[cfg_attr(feature = "inline-more", inline)]
+    fn try_from_usize(int: usize) -> Option<Self> {
+        if int < u32::max_value() as usize {
+            // Safety: The integer is less than the max value and then incremented by one, meaning that
+            // is is impossible for a zero to inhabit the NonZeroU32
+            unsafe { Some(Self(NonZeroU32::new_unchecked(int as u32 + 1))) }
+        } else {
+            None
+        }
+    }
+}
+
+impl Into<NodeIndex> for NicheKey {
+    #[cfg_attr(feature = "inline-more", inline)]
+    fn into(self) -> NodeIndex {
+        NodeIndex::new(self.0.get() as usize - 1)
+    }
+}
+
+impl Into<EdgeIndex> for NicheKey {
+    #[cfg_attr(feature = "inline-more", inline)]
+    fn into(self) -> EdgeIndex {
+        EdgeIndex::new(self.0.get() as usize - 1)
+    }
+}
+
+impl From<NodeIndex> for NicheKey {
+    #[cfg_attr(feature = "inline-more", inline)]
+    fn from(index: NodeIndex) -> Self {
+        NicheKey(NonZeroU32::new(index.index() as u32 + 1).unwrap())
+    }
+}
+
+impl From<&NodeIndex> for NicheKey {
+    #[cfg_attr(feature = "inline-more", inline)]
+    fn from(index: &NodeIndex) -> Self {
+        NicheKey(NonZeroU32::new(index.index() as u32 + 1).unwrap())
+    }
+}
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct Triple<'a> {
-    head: &'a str,
-    tail: &'a str,
-    relation: &'a str,
-}
-impl fmt::Display for Triple<'_> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}\t{}\t{}", self.head, self.relation, self.tail)
-    }
+struct Triple {
+    head: NicheKey,
+    tail: NicheKey,
+    relation: NicheKey,
 }
 
 pub fn process(name: &str) -> Result<(), Box<dyn std::error::Error>> {
     let mut triples = vec![];
     let mut file = String::new();
+    let mut rodeo = Rodeo::<NicheKey>::new();
     fs::File::open(name)?.read_to_string(&mut file)?;
     println!("Reading file...");
     let bar = ProgressBar::new_spinner();
@@ -32,28 +90,30 @@ pub fn process(name: &str) -> Result<(), Box<dyn std::error::Error>> {
         let relation = parts.next().ok_or("Wrong format!")?;
         let tail = parts.next().ok_or("Wrong format!")?;
         triples.push(Triple {
-            head: head,
-            tail: tail,
-            relation: relation,
+            head: rodeo.get_or_intern(head),
+            tail: rodeo.get_or_intern(tail),
+            relation: rodeo.get_or_intern(relation),
         });
     }
+    let rodeo = rodeo.into_reader();
     bar.finish();
     println!("{} triples", triples.len());
     println!("Building maps...");
-    let bar = ProgressBar::new_spinner();
-    bar.enable_steady_tick(100);
     // let head_map = DashMap::new();
-    let relation_map: DashMap<&str, HashMap<&str, Vec<&Triple>>> = DashMap::new();
-    triples.par_iter().for_each(|triple| {
-        relation_map
-            .entry(triple.head)
-            .or_default()
-            .entry(triple.relation)
-            .or_default()
-            .push(triple);
-    });
-    bar.finish();
-    println!("Map built with total size {}", relation_map.len());
+    // let mut relation_map: HashMap<&str, HashMap<&str, Vec<&Triple>>> = HashMap::new();
+    // triples.iter().for_each(|triple| {
+    //     relation_map
+    //         .entry(triple.head)
+    //         .or_default()
+    //         .entry(triple.relation)
+    //         .or_default()
+    //         .push(triple);
+    // });
+    // bar.finish();
+    let graph = Graph::<NicheKey, NicheKey, Directed>::from_edges(
+        triples.iter().map(|t| (t.head, t.tail, t.relation)),
+    );
+    println!("Map built with total size {}", graph.node_count());
     println!("Finding neighbor...");
     let bar = ProgressBar::new(triples.len() as u64);
     bar.set_style(
@@ -66,62 +126,39 @@ pub fn process(name: &str) -> Result<(), Box<dyn std::error::Error>> {
 
     triples
         .par_iter()
-        .map(|triple| -> (&Triple, Option<(&Triple, f64)>) {
+        .map(|triple| -> (&Triple, Option<(Triple, f64)>) {
             bar.inc(1);
-            if let Some(_) = relation_map.get(triple.tail) {
-                let mut map: HashMap<&str, HashMap<&str, HashSet<&Triple>>> = HashMap::new();
-                relation_map.get(triple.head).map(|h| {
-                    h.iter().for_each(|r1| {
-                        r1.1.iter().for_each(|t1| {
-                            relation_map.get(t1.tail).map(|t2| {
-                                t2.iter().for_each(|r2| {
-                                    // let t = ;
-                                    // r2.1.iter().for_each(|t2| {
-                                    //     map.entry(r1.0).or_default().entry(r2.0).or_default().insert(t2);
-                                    // });
-                                    map.entry(r1.0)
-                                        .or_default()
-                                        .entry(r2.0)
-                                        .or_default()
-                                        .extend(r2.1);
-                                });
-                            });
-                        });
-                    });
-                });
-                let total_length = map
-                    .values()
-                    .map(|v| v.values().map(|v| v.len()).sum::<usize>())
-                    .sum::<usize>();
-                // println!("{:?}", map);
-                let max = map
-                    .iter()
-                    .filter_map(|m| {
-                        // find max in m.1
-                        let mm = m.1.iter().max_by_key(|d| d.1.len());
-                        if let Some((r, l)) = mm {
-                            Some((m.0, r, l.len()))
-                        } else {
-                            None
-                        }
-                    })
-                    .max_by_key(|d| d.2);
-
-                if let Some(r1) = max {
-                    if (r1.2 == 0) {
-                        (triple, None)
-                    } else {
-                        let t = map.get(r1.0).unwrap().get(r1.1).unwrap();
-                        (
-                            triple,
-                            Some((
-                                t.iter().take(1).next().unwrap(),
-                                r1.2 as f64 / total_length as f64,
-                            )),
-                        )
+            if !graph.neighbors(triple.tail.into()).any(|x| true) {
+                return (triple, None);
+            }
+            let mut set: FxHashMap<NicheKey, FxHashSet<NodeIndex>> = FxHashMap::default();
+            for e1 in graph.edges(triple.head.into()) {
+                if *e1.weight() == triple.relation {
+                    for e2 in graph.edges(e1.target()) {
+                        set.entry(*e2.weight()).or_default().insert(e2.target());
                     }
-                } else {
-                    (triple, None)
+                }
+            }
+            let max = set.iter().max_by_key(|(_, v)| v.len());
+            let sum = set.iter().map(|(_, v)| v.len()).sum::<usize>();
+            if let Some((k, v)) = max {
+                // (triple, Some((triple, v.len() as f64)))
+                if v.len() == 0 {
+                    (triple, Option::<(Triple, f64)>::None);
+                }
+                {
+                    let first = v.iter().next().unwrap();
+                    (
+                        triple,
+                        Some((
+                            Triple {
+                                head: triple.tail,
+                                relation: *k,
+                                tail: first.into(),
+                            },
+                            v.len() as f64 / sum as f64,
+                        )),
+                    )
                 }
             } else {
                 (triple, None)
@@ -129,20 +166,32 @@ pub fn process(name: &str) -> Result<(), Box<dyn std::error::Error>> {
         })
         .collect::<Vec<_>>()
         .iter()
-        .for_each(|triple| match triple {
-            (triple, Some(neighbor)) => {
-                writer
-                    .write(
-                        format!(
-                            "{}\t{}\t{}\t{}\n",
-                            triple, neighbor.0.relation, neighbor.0.tail, neighbor.1
-                        )
-                        .as_bytes(),
-                    )
-                    .unwrap();
-            }
-            (triple, None) => {
-                writer.write(format!("{}\n", triple).as_bytes()).unwrap();
+        .for_each(|triple| {
+            unsafe {
+                match triple {
+                    (triple, Some(neighbor)) => {
+                        writer
+                            .write(
+                                format!(
+                                    "{}\t{}\t{}\t{}\t{}\t{}\n",
+                                    rodeo.resolve_unchecked(&triple.head),
+                                    rodeo.resolve_unchecked(&triple.relation),
+                                    rodeo.resolve_unchecked(&triple.tail),
+                                    rodeo.resolve_unchecked(&neighbor.0.relation),
+                                    rodeo.resolve_unchecked(&neighbor.0.tail),
+                                    neighbor.1
+                                )
+                                .as_bytes(),
+                            )
+                            .unwrap();
+                    }
+                    (triple, None) => {
+                        writer.write(format!("{}\t{}\t{}\n", 
+                        rodeo.resolve_unchecked(&triple.head),
+                        rodeo.resolve_unchecked(&triple.relation),
+                        rodeo.resolve_unchecked(&triple.tail),).as_bytes()).unwrap();
+                    }
+                }
             }
         });
     writer.flush()?;
